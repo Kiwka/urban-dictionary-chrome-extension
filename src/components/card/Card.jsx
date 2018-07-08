4590e16a14b2c0bee2508ebe17f6694721f360bf
Olena Sovyn <olenasovyn@gmail.com>
2018-07-08 17:08:30 +0100

    init Card existance

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+class Card extends React.Component {
+  render() {
+    return (<div className="Card">
+      Card is here
+    </div>);
+  }
+}
+
+export default Card;

5007b43c65e2bf97d7e1dc7b7cb5e35c979c07b1
Olena Sovyn <olenasovyn@gmail.com>
2018-07-08 17:34:10 +0100

    read card data from endpoint

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import './Card.css';
 
 class Card extends React.Component {
+  state = {
+    cards: []
+  }
+  componentDidMount() {
+    fetch('http://api.urbandictionary.com/v0/random')
+    .then(res => res.json())
+    .then(data => this.setState({cards: data.list}));
+  }
   render() {
-    return (<div className="Card">
-      Card is here
-    </div>);
+    const {cards} = this.state;
+    if (!this.state.cards || !cards.length) {
+        return (<div>Loading...</div>);
+    }
+
+    console.log(this.state);
+    return this.state.cards.map(card => (<article className="Card">
+      <header>{card.word}</header>
+      <div>{card.definition}</div>
+      <a href={card.permalink}>{card.permalink}</a>
+    </article>));
   }
 }
 
